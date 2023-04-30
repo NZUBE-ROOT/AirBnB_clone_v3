2d9933a38203fd4f832b661fc6ef02d293aa8daa
NZUBE-ROOT <momanzubeolaedo@gmail.com>
2023-04-30 14:55:53 +0000

    bash

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,34 +1,37 @@
 #!/usr/bin/python3
-'''
-    flask with general routes
-    routes:
-        /status:    display "status":"OK"
-        /stats:     dispaly total for all classes
-'''
+"""Contains the routes for the json objects
+Routes:
+    /status: returns the 'OK' status
+    /stats: returns the total count of the objects in database
+Usage:
+    python3 -m api.v1.app
+"""
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
-@app_views.route("/status")
-def status():
-    '''
-        return JSON of OK status
-    '''
-    return jsonify({'status': 'OK'})
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def static():
+    """returns json object"""
+    return jsonify(status="OK")
 
 
-@app_views.route("/stats")
-def storage_counts():
-    '''
-        return counts of all classes in storage
-    '''
-    cls_counts = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
-    }
-    return jsonify(cls_counts)
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """Returns the count for all items in database"""
+    obj = {
+            'amenities': storage.count(Amenity),
+            'cities': storage.count(City),
+            'places': storage.count(Place),
+            'reviews': storage.count(Review),
+            'states': storage.count(State),
+            'users': storage.count(User)
+            }
+    return jsonify(obj)
